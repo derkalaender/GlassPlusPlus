8f6dabaed0117d77a1613130d2bcf38d846ec641
derkalaender <git@derkalaender.de>
2020-04-14 13:17:04 +0200

    Simplify glass shard recipe and make it "special"

--- src/main/kotlin/de/derkalaender/glasspp/recipe/GlassShardRecipe.kt
+++ src/main/kotlin/de/derkalaender/glasspp/recipe/GlassShardRecipe.kt
@@ -1,74 +1,33 @@
 package de.derkalaender.glasspp.recipe
 
-import com.google.gson.JsonObject
 import de.derkalaender.glasspp.Registry
-import de.derkalaender.glasspp.glass.GlassTypes
 import de.derkalaender.glasspp.items.GlassShard
 import de.derkalaender.glasspp.util.toItemStack
 import net.minecraft.inventory.CraftingInventory
-import net.minecraft.item.ItemStack
-import net.minecraft.item.crafting.IRecipeSerializer
-import net.minecraft.item.crafting.Ingredient
-import net.minecraft.item.crafting.ShapedRecipe
-import net.minecraft.network.PacketBuffer
-import net.minecraft.util.NonNullList
+import net.minecraft.item.crafting.SpecialRecipe
+import net.minecraft.item.crafting.SpecialRecipeSerializer
 import net.minecraft.util.ResourceLocation
-import net.minecraftforge.registries.ForgeRegistryEntry
+import net.minecraft.world.World
+import java.util.function.Function
 
 class GlassShardRecipe(
-    id: ResourceLocation,
-    group: String,
-    recipeWidth: Int,
-    recipeHeight: Int,
-    ingredients: NonNullList<Ingredient>,
-    private val recipeOutput: ItemStack
-) : ShapedRecipe(id, group, recipeWidth, recipeHeight, ingredients, recipeOutput) {
-    override fun getSerializer() = Registry.GLASS_SHARD_RECIPE_SERIALIZER.get()
-
-    override fun getRecipeOutput(): ItemStack = ItemStack.EMPTY
-
-    override fun isDynamic() = true
-
-    override fun getCraftingResult(inv: CraftingInventory): ItemStack {
-        return if (recipeOutput.item.registryName in GlassTypes.getAll().map { it.getResourceLocation() }) {
-            val allStacks = mutableListOf<ItemStack>()
-            for (x in 0 ..inv.width) {
-                for (y in 0..inv.height) {
-                    allStacks.add(inv.getStackInSlot(x + y * width))
-                }
-            }
-            val actualGlassTypes = allStacks.filter { it.item is GlassShard }.map { GlassShard.getGlassType(it) }.distinct()
-            if (actualGlassTypes.size == 1) {
-                return actualGlassTypes.first().getItem().toItemStack(recipeOutput.count)
-            } else {
-                ItemStack.EMPTY
-            }
-        } else {
-            recipeOutput.copy()
-        }
+    id: ResourceLocation
+) : SpecialRecipe(id) {
+    companion object {
+        val SERIALIZER = SpecialRecipeSerializer(Function(::GlassShardRecipe))
     }
 
-    class Serializer : ForgeRegistryEntry<IRecipeSerializer<*>>(), IRecipeSerializer<GlassShardRecipe> {
+    override fun matches(inv: CraftingInventory, worldIn: World) =
+        inv.getAllSlotContents().filter { it.item == Registry.GLASS_SHARD.get() }.map { GlassShard.getGlassType(it) }
+            .distinct().size == 1
 
-        private val delegatedSerializer = ShapedRecipe.Serializer()
+    override fun getCraftingResult(inv: CraftingInventory) =
+        inv.getAllSlotContents().first { it.item == Registry.GLASS_SHARD.get() }
+            .let { GlassShard.getGlassType(it).getItem().toItemStack(1) }
 
-        override fun read(recipeId: ResourceLocation, json: JsonObject): GlassShardRecipe {
+    private fun CraftingInventory.getAllSlotContents() = (0 until sizeInventory).map { getStackInSlot(it) }
 
-            return delegatedSerializer.read(recipeId, json).toGlassShardRecipe()
-        }
+    override fun canFit(width: Int, height: Int) = width >= 2 && height >= 2
 
-        override fun read(recipeId: ResourceLocation, buffer: PacketBuffer): GlassShardRecipe? {
-            return delegatedSerializer.read(recipeId, buffer)?.toGlassShardRecipe()
-        }
-
-        override fun write(buffer: PacketBuffer, recipe: GlassShardRecipe) {
-            delegatedSerializer.write(buffer, recipe.toShapedRecipe())
-        }
-
-        private fun ShapedRecipe.toGlassShardRecipe() =
-            GlassShardRecipe(id, group, recipeWidth, recipeHeight, ingredients, recipeOutput)
-
-        private fun GlassShardRecipe.toShapedRecipe() =
-            ShapedRecipe(id, group, recipeWidth, recipeHeight, ingredients, recipeOutput)
-    }
+    override fun getSerializer() = SERIALIZER
 }
