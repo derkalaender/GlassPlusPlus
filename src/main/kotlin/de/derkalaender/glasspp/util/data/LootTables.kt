edffde2e2870b8b7c82b391b8e063e41933f4f0b
derkalaender <git@derkalaender.de>
2020-04-18 11:24:29 +0200

    Make shards not drop with silk touch

--- src/main/kotlin/de/derkalaender/glasspp/util/data/LootTables.kt
+++ src/main/kotlin/de/derkalaender/glasspp/util/data/LootTables.kt
@@ -3,11 +3,12 @@ package de.derkalaender.glasspp.util.data
 import com.mojang.datafixers.util.Pair
 import de.derkalaender.glasspp.Registry
 import de.derkalaender.glasspp.util.rl
-import java.util.function.BiConsumer
-import java.util.function.Consumer
-import java.util.function.Supplier
+import net.minecraft.advancements.criterion.EnchantmentPredicate
+import net.minecraft.advancements.criterion.ItemPredicate
+import net.minecraft.advancements.criterion.MinMaxBounds
 import net.minecraft.data.DataGenerator
 import net.minecraft.data.LootTableProvider
+import net.minecraft.enchantment.Enchantments
 import net.minecraft.util.ResourceLocation
 import net.minecraft.world.storage.loot.ItemLootEntry
 import net.minecraft.world.storage.loot.LootParameterSet
@@ -17,6 +18,11 @@ import net.minecraft.world.storage.loot.LootTable
 import net.minecraft.world.storage.loot.LootTableManager
 import net.minecraft.world.storage.loot.RandomValueRange
 import net.minecraft.world.storage.loot.ValidationTracker
+import net.minecraft.world.storage.loot.conditions.Inverted
+import net.minecraft.world.storage.loot.conditions.MatchTool
+import java.util.function.BiConsumer
+import java.util.function.Consumer
+import java.util.function.Supplier
 
 class LootTables(gen: DataGenerator) : LootTableProvider(gen) {
     override fun getTables(): MutableList<Pair<Supplier<Consumer<BiConsumer<ResourceLocation, LootTable.Builder>>>, LootParameterSet>> {
@@ -38,6 +44,19 @@ class LootTables(gen: DataGenerator) : LootTableProvider(gen) {
                 rl("glass_shard/global"), LootTable.builder().addLootPool(
                     LootPool.builder()
                         .name("pool1")
+                        .acceptCondition(
+                            Inverted.builder(
+                                MatchTool.builder(
+                                    ItemPredicate.Builder.create()
+                                        .enchantment(
+                                            EnchantmentPredicate(
+                                                Enchantments.SILK_TOUCH,
+                                                MinMaxBounds.IntBound.atLeast(1)
+                                            )
+                                        )
+                                )
+                            )
+                        )
                         .rolls(RandomValueRange(1f, 3f))
                         .addEntry(ItemLootEntry.builder(Registry.GLASS_SHARD::get))
                 )
